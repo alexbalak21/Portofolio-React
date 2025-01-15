9780435b304b46ce1bc446b8dbd91a9fd2156d0e
alexbalak21 <alex.balak@outlook.com>
2025-01-15 15:08:47 +0100

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,41 @@
 import React from "react"
+import menuIcon from "../assets/menuIcon.png"
+import closeIcon from "../assets/closeIcon.png"
 
 function Navbar() {
-  return <div>Navbar</div>
+  const [menuOpen, setMenuOpen] = React.useState(false)
+  return (
+    <nav className="navbar">
+      <a href="/" className="navbar__title">
+        Portfolio
+      </a>
+      <div className="navbar__menu">
+        <img
+          className="navbar__menuBtn"
+          src={menuOpen ? closeIcon : menuIcon}
+          alt="menu"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`navbar__menuItems ${menuOpen && "navbar__menuOpen"}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#project">Project</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
