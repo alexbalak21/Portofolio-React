475325b85c46a4be65ada1075a3ad4f332fa0b90
alexbalak21 <alex.balak@outlook.com>
2025-01-16 12:04:26 +0100

    Experience component done.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -20,8 +20,24 @@ function Experience() {
         </div>
         <ul className="experience__history">
           {history.map((historyItem, id) => {
-            // Video time : 21:00
-            // url : https://www.youtube.com/watch?v=7KapMI0Vu-s
+            return (
+              <li className="experience__historyItem" key={id}>
+                <img src={historyItem.imageSrc} alt={historyItem.title} />
+                <div className="experience__historyItemDetails">
+                  <h3>
+                    {historyItem.role} at {historyItem.organisation}
+                  </h3>
+                  <p>
+                    {historyItem.startDate} - {historyItem.endDate}
+                  </p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>
+                    })}
+                  </ul>
+                </div>
+              </li>
+            )
           })}
         </ul>
       </div>
