42894bb6cf55b6d9d1d6cfa5c77c5a129e6efde5
alexbalak21 <alex.balak@outlook.com>
2026-02-24 18:09:29 +0100

    included compression

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from "vite-plugin-compression"
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  base: '/', // homepage served at root
+  plugins: [react(), compression({ algorithm: "brotliCompress" })],
   build: {
+    outDir: 'build',
+    emptyOutDir: true,
+    assetsDir: 'assets',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name].[hash][extname]'
       }
-    },
-    assetsDir: 'assets'
+    }
   }
 })
